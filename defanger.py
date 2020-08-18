a035c24b989643d9be9ed115efa6225ec16436bb
kr888tr <kr888tr@protonmail.com>
2020-08-18 15:23:50 -0700

    pseudocode added

--- defanger.py
+++ defanger.py
@@ -1,1 +1,22 @@
 print("This is the initial commit to get the repo started")
+
+
+# Pseudocode Outline
+
+# Specify Command Line Arguments
+
+# Initialize parser
+
+# Take user input .txt file
+
+# read lines of file into list
+
+# defang urls in list
+    # replace . with [.]
+    # replace https:// with hxxps ://
+
+# write list into lines of new file
+
+# close file
+
+# exit gracefully
